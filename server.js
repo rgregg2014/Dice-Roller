692a72a95812010eea23051847698755416c61da
Rachel Gregg <rgregg2014@gmail.com>
2021-08-15 18:35:46 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+//DEPENDENCIES ===================================================================
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+//PORTS ==========================================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
