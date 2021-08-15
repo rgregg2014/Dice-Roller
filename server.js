5fe4728a9f8fe53e82a496091157b95111d16e44
Rachel Gregg <rgregg2014@gmail.com>
2021-08-15 18:45:01 -0400

    added DevDependencies, init server for Heroku deployment

--- server.js
+++ server.js
@@ -6,3 +6,17 @@ const path = require("path");
 //PORTS ==========================================================================
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//DATA HANDLING ==================================================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.search(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send(`<h1> Hello World </h1>`);
+});
+
+//SERVER LISTENER ================================================================
+app.listen(PORT, () =>
+  console.log(`App listening on PORT http://localhost:${PORT}`)
+);
